a88e4d074d9f57476633dbaf092b067ff78fce26
Geo Mealer <gmealer@mozilla.com>
2010-12-05 16:01:12 -0800

    Parity with whimboo, + a couple of different asserts

--- modules/assert.js
+++ modules/assert.js
@@ -1,6 +1,38 @@
-exports.is = function is(condition1, condition2, message) {
-  if (condition1 !== condition2) {
-    throw new Error(message + " - got " + condition1 + ", expected " + condition2);
-  }
+// This will get used a lot.
+var $ = mozmill.controller;
+
+/**
+ * Succeeds if the supplied value is true
+ *
+ * @param value
+ *        Value to test for truth
+ * @param message
+ *        Message to include with result
+ */
+function ok(value, message) {
+  message = message + " - " + String(value);
+  $.assert(function { 
+    return value;
+  }, message);
 }
 
+/**
+ * Succeeds if got == expected
+ *
+ * @param got
+ *        Actual value
+ * @param expected
+ *        Expected value
+ * @param message
+ *        Message to include with result
+ */
+function is(got, expected, message) {
+  message = message + " - actual: " + String(condition1) + ", expected: " + String(condition2);
+  $.assert(function {
+    condition1 == condition2
+  }, message);
+}
+
+// Exported functions
+exports.ok = ok;
+exports.is = is;
