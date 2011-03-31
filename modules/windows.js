55b5baf11c86716ca3418af6f08d5128497c932a
whimboo <hskupin@mozilla.com>
2011-03-31 14:29:23 +0200

    Bug 639870 - Add methods to retrieve specific windows

---
+++ modules/windows.js
@@ -0,0 +1,183 @@
+/* ***** BEGIN LICENSE BLOCK *****
+ * Version: MPL 1.1/GPL 2.0/LGPL 2.1
+ *
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * The Original Code is MozMill Test code.
+ *
+ * The Initial Developer of the Original Code is the Mozilla Foundation.
+ * Portions created by the Initial Developer are Copyright (C) 2011
+ * the Initial Developer. All Rights Reserved.
+ *
+ * Contributor(s):
+ *   Henrik Skupin <mail@hskupin.info>
+ *
+ * Alternatively, the contents of this file may be used under the terms of
+ * either the GNU General Public License Version 2 or later (the "GPL"), or
+ * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+ * in which case the provisions of the GPL or the LGPL are applicable instead
+ * of those above. If you wish to allow use of your version of this file only
+ * under the terms of either the GPL or the LGPL, and not to allow others to
+ * use your version of this file under the terms of the MPL, indicate your
+ * decision by deleting the provisions above and replace them with the notice
+ * and other provisions required by the GPL or the LGPL. If you do not delete
+ * the provisions above, a recipient may use your version of this file under
+ * the terms of any one of the MPL, the GPL or the LGPL.
+ *
+ * ***** END LICENSE BLOCK ***** */
+
+
+/**
+ * @namespace Helper methods for the handling of all kinds of windows
+ */
+var windows = exports;
+
+// Include necessary modules
+var stack = require('stack');
+var services = require('services');
+
+
+/**
+ * Internal method to retrieve a DOM window by a given sorting order and type.
+ *
+ * @private
+ * @param {Function} aWindowCallback Function which retrieves a list of windows.
+ * @param {Function} [aFilterCallback] Function which is used to filter windows.
+ * @param {Object} [aValue] Value to pass to the filter function.
+ *
+ * @returns {DOMWindow} The window found.
+ */
+function _getWindow(aWindowCallback, aFilterCallback, aValue) {
+  let windows;
+
+  if (aFilterCallback) {
+    windows = aWindowCallback(function (window) {
+      return aFilterCallback(window, aValue);
+    });
+  }
+  else {
+    windows = aWindowCallback();
+  }
+
+  return windows.length ? windows[0] : null;
+}
+
+
+/**
+ * Internal method to retrieve a list of DOM windows by a given sorting order
+ * and filter.
+ *
+ * @private
+ * @param {nsISimpleEnumerator} aEnumerator Window enumerator to use.
+ * @param {Function} [aFilterCallback] Function which is used to filter windows.
+ *
+ * @returns {DOMWindow[]} The windows found.
+ */
+function _getWindows(aEnumerator, aFilterCallback) {
+  let windows = [];
+
+  while (aEnumerator.hasMoreElements()) {
+    let window = aEnumerator.getNext();
+
+    if (!aFilterCallback || aFilterCallback(window)) {
+      windows.push(window);
+    }
+  }
+
+  return windows.reverse();
+}
+
+
+/**
+ * Callback to filter entries in a window list by window title
+ *
+ * @param {DOMWindow} aWindow Window to check.
+ * @param {String} aTitle Title of the window.
+ *
+ * @returns {Boolean} True if the condition is met.
+ */
+function filterByTitle(aWindow, aTitle) {
+  return aWindow.document.title === aTitle;
+}
+
+
+/**
+ * Callback to filter entries in a window list by window type
+ *
+ * @param {DOMWindow} aWindow Window to check.
+ * @param {String} aType Type of the window.
+ *
+ * @returns {Boolean} True if the condition is met.
+ */
+function filterByType(aWindow, aType) {
+  return aWindow.document.documentElement.getAttribute("windowtype") === aType;
+}
+
+
+/**
+ * Retrieves a sorted list of open windows based on their age (newest to oldest).
+ *
+ * @param {Function} [aFilterCallback] Function which is used to filter windows.
+ *
+ * @returns {DOMWindow[]} List of windows.
+ */
+function getLastOpenedWindows(aFilterCallback) {
+  return _getWindows(services.wm.getEnumerator(""), aFilterCallback);
+}
+
+
+/**
+ * Retrieves a sorted list of open windows based on their z order (most recent first).
+ *
+ * @param {Function} [aFilterCallback] Function which is used to filter windows.
+ *
+ * @returns {DOMWindow[]} List of windows.
+ */
+function getMostRecentWindows(aFilterCallback) {
+  return _getWindows(services.wm.getZOrderDOMWindowEnumerator("", false),
+                     aFilterCallback);
+}
+
+
+/**
+ * Retrieves the last opened window.
+ *
+ * @param {Function} [aFilterCallback] Function which is used to filter windows.
+ * @param {Object} [aValue] Value to pass to the filter function.
+ *
+ * @returns {DOMWindow} The window.
+ */
+function getLastOpenedWindow(aFilterCallback, aValue) {
+  return _getWindow(getLastOpenedWindows, aFilterCallback, aValue);
+}
+
+/**
+ * Retrieves the most recent window.
+ *
+ * @param {Function} [aFilterCallback] Function which is used to filter windows.
+ * @param {Object} [aValue] Value to pass to the filter function.
+ *
+ * @returns {DOMWindow} The window.
+ */
+function getMostRecentWindow(aFilterCallback, aValue) {
+  return _getWindow(getMostRecentWindows, aFilterCallback, aValue);
+}
+
+
+// Export of methods
+windows.filterByTitle = filterByTitle;
+windows.filterByType = filterByType;
+
+windows.getLastOpenedWindow = getLastOpenedWindow;
+windows.getLastOpenedWindows = getLastOpenedWindows;
+windows.getMostRecentWindow = getMostRecentWindow;
+windows.getMostRecentWindows = getMostRecentWindows;
+
